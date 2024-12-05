cb653a06644eca850c690c2d9afe064605548d10
arthur2332 <44934291+arthur2332@users.noreply.github.com>
2024-12-05 11:23:34 -0300

    Add files via upload

---
+++ Aula 2 - tutu/Aula02-tutu.py
@@ -0,0 +1,11 @@
+print('Bom dia galerinha')
+
+nome_usuario = 'Tutu'# tipo -> = string
+idade_usuario = 19 # tipo -> int = integer não tem aspas por não ser texto
+altura_usuario = 1.71 # tipo -> float = flutuante
+maior_idade = True # tipo -> bool = boolean
+
+print ('Seja bem vindo',nome_usuario)
+print('Sua idade é', idade_usuario, 'anos')
+Print('Sua altura é', altura_usuario, 'metros.')
+Print("Você é maior de idade?", maior_idade)
